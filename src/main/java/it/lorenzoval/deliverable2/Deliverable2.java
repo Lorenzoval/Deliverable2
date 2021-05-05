433e564e5625632fabf03b2e5cc6bd9b051461ae
Lorenzo Valeriani <lorenzoval98@gmail.com>
2021-05-05 22:56:02 +0200

    Deliverable2 #11
    Closes #11
    Signed-off-by: Lorenzo Valeriani <lorenzoval98@gmail.com>

--- src/main/java/it/lorenzoval/deliverable2/Deliverable2.java
+++ src/main/java/it/lorenzoval/deliverable2/Deliverable2.java
@@ -4,8 +4,11 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Deliverable2 {
 
@@ -13,14 +16,18 @@ public class Deliverable2 {
         File outFile = new File(project.getProjectName() + ".csv");
         List<String> lines = new ArrayList<>();
         StringBuilder line = new StringBuilder();
-        lines.add("Version,File Name");
+        lines.add("Version,File Name,LOC");
         List<Release> releases = JIRAHandler.getReleases(project);
         for (Release release : releases) {
             GitHandler.changeRelease(project, release);
             List<String> files = GitHandler.getFiles(project);
             for (String file : files) {
                 line.setLength(0);
-                line.append(release.getName()).append(",").append(file);
+                long loc;
+                try (Stream<String> fileLines = Files.lines(Paths.get(project.getProjectName(), file))) {
+                    loc = fileLines.count();
+                }
+                line.append(release.getName()).append(",").append(file).append(",").append(loc);
                 lines.add(line.toString());
             }
         }
