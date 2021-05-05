71c1312d68da06f9c7c8d222bcb078c876f95374
Lorenzo Valeriani <lorenzoval98@gmail.com>
2021-05-05 22:30:26 +0200

    Deliverable2 #10
    Closes #10
    Signed-off-by: Lorenzo Valeriani <lorenzoval98@gmail.com>

--- src/main/java/it/lorenzoval/deliverable2/Deliverable2.java
+++ src/main/java/it/lorenzoval/deliverable2/Deliverable2.java
@@ -1,43 +1,39 @@
 package it.lorenzoval.deliverable2;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Deliverable2 {
 
-    private static final Logger logger = Logger.getLogger(Deliverable2.class.getName());
+    public static void buildDataset(Project project) throws IOException, InterruptedException {
+        File outFile = new File(project.getProjectName() + ".csv");
+        List<String> lines = new ArrayList<>();
+        StringBuilder line = new StringBuilder();
+        lines.add("Version,File Name");
+        List<Release> releases = JIRAHandler.getReleases(project);
+        for (Release release : releases) {
+            GitHandler.changeRelease(project, release);
+            List<String> files = GitHandler.getFiles(project);
+            for (String file : files) {
+                line.setLength(0);
+                line.append(release.getName()).append(",").append(file);
+                lines.add(line.toString());
+            }
+        }
+        FileUtils.writeLines(outFile, lines);
+    }
 
     public static void main(String[] args) throws IOException, InterruptedException {
         Project syncope = new Syncope();
         Project bookkeeper = new Bookkeeper();
         GitHandler.cloneOrPull(syncope);
         GitHandler.cloneOrPull(bookkeeper);
-
-        /* List<Issue> bugs = JIRAHandler.getBugs(syncope);
-
-        for (Issue bug : bugs) {
-            logger.log(Level.INFO, bug.getKey());
-        } */
-
-        List<Release> syncopeReleases = JIRAHandler.getReleases(syncope);
-        int i = 0;
-
-        for (Release release : syncopeReleases) {
-            i++;
-            logger.log(Level.INFO, "{0} {1}", new Object[]{release.getName(), release.getReleaseDate()});
-        }
-        logger.log(Level.INFO, "Total releases: {0}", i);
-
-        List<Release> bookkeeperReleases = JIRAHandler.getReleases(bookkeeper);
-        i = 0;
-
-        for (Release release : bookkeeperReleases) {
-            i++;
-            logger.log(Level.INFO, "{0} {1}", new Object[]{release.getName(), release.getReleaseDate()});
-        }
-        logger.log(Level.INFO, "Total releases: {0}", i);
+        buildDataset(syncope);
+        buildDataset(bookkeeper);
     }
 
 }
--- src/main/java/it/lorenzoval/deliverable2/GitHandler.java
+++ src/main/java/it/lorenzoval/deliverable2/GitHandler.java
@@ -1,8 +1,13 @@
 package it.lorenzoval.deliverable2;
 
+import org.apache.commons.io.IOUtils;
+
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -28,7 +33,7 @@ public class GitHandler {
             } else {
                 logMsg = MessageFormat.format("Updating {0} source code",
                         projectName);
-                pb = new ProcessBuilder("git", "pull");
+                pb = new ProcessBuilder("git", "fetch", "--all");
                 pb.directory(file);
             }
         } else {
@@ -42,4 +47,26 @@ public class GitHandler {
         pr.waitFor();
     }
 
+    public static void changeRelease(Project project, Release release) throws IOException, InterruptedException {
+        String projectName = project.getProjectName();
+        String tagName = MessageFormat.format(project.getReleaseString(), release.getName());
+        File file = new File(projectName);
+        ProcessBuilder pb = new ProcessBuilder("git", "checkout",
+                MessageFormat.format("tags/{0}", tagName));
+        pb.directory(file);
+        pb.inheritIO();
+        Process pr = pb.start();
+        pr.waitFor();
+    }
+
+    public static List<String> getFiles(Project project) throws IOException, InterruptedException {
+        String projectName = project.getProjectName();
+        File file = new File(projectName);
+        ProcessBuilder pb = new ProcessBuilder("git", "ls-files", "*.java");
+        pb.directory(file);
+        Process pr = pb.start();
+        String files = IOUtils.toString(pr.getInputStream(), StandardCharsets.UTF_8);
+        pr.waitFor();
+        return Arrays.asList(files.split("\n"));
+    }
 }
