e468dcac639ea0453fd9deba233a2b7ca7ba44d9
Lorenzo Valeriani <lorenzoval98@gmail.com>
2021-09-18 18:45:19 +0200

    Deliverable2 #13
    Closes #13
    Signed-off-by: Lorenzo Valeriani <lorenzoval98@gmail.com>

--- src/main/java/it/lorenzoval/deliverable2/Deliverable2.java
+++ src/main/java/it/lorenzoval/deliverable2/Deliverable2.java
@@ -7,11 +7,75 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
+import static java.lang.Math.min;
+
 public class Deliverable2 {
+
+    private static final Logger logger = Logger.getLogger(Deliverable2.class.getName());
+
+    public static boolean isHigherVersionNumber(String version1, String version2) {
+        List<String> version1Digits = Arrays.asList(version1.split("\\."));
+        List<String> version2Digits = Arrays.asList(version2.split("\\."));
+
+        int minSize = min(version1Digits.size(), version2Digits.size());
+        boolean lower = false;
+        boolean higher = false;
+        for (int i = 0; i < minSize; i++) {
+            if (Integer.parseInt(version1Digits.get(i)) < Integer.parseInt(version2Digits.get(i))) {
+                lower = true;
+            } else if (Integer.parseInt(version1Digits.get(i)) > Integer.parseInt(version2Digits.get(i))) {
+                higher = true;
+            }
+            if (lower || higher)
+                break;
+        }
+        if (!lower && !higher)
+            return version1Digits.size() >= version2Digits.size();
+        else
+            return higher;
+    }
+
+    public static List<Release> dropBackwardCompatibility(List<Release> releases) {
+        Release prev = null;
+        final Pattern pattern = Pattern.compile("\\d+[.\\d]*");
+        List<Release> retList = new ArrayList<>();
+        for (Release release : releases) {
+            if (prev == null) {
+                prev = release;
+                retList.add(release);
+                continue;
+            }
+            String prevNumber = prev.getName();
+            String currNumber = release.getName();
+            Matcher matcher = pattern.matcher(prevNumber);
+            if (matcher.find())
+                prevNumber = matcher.group();
+            else
+                logger.log(Level.SEVERE, "Matching not found");
+            matcher = pattern.matcher(currNumber);
+            if (matcher.find())
+                currNumber = matcher.group();
+            else
+                logger.log(Level.SEVERE, "Matching not found");
+            if (isHigherVersionNumber(currNumber, prevNumber)) {
+                prev = release;
+                retList.add(release);
+            } else {
+                logger.log(Level.INFO, "Release {0} released after {1}, discarded", new Object[]{release.getName(),
+                        prev.getName()});
+            }
+        }
+        return retList;
+    }
 
     public static void buildDataset(Project project) throws IOException, InterruptedException {
         File outFile = new File(project.getProjectName() + ".csv");
@@ -20,6 +84,7 @@ public class Deliverable2 {
         lines.add("Version,File Name,LOC");
         List<Release> releases = JIRAHandler.getReleases(project);
         Collections.sort(releases);
+        releases = dropBackwardCompatibility(releases);
         for (Release release : releases) {
             GitHandler.changeRelease(project, release);
             List<String> files = GitHandler.getFiles(project);
